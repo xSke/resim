93fa4c11559f95f66ff8f770bcdaf69b08e03196
Astrid <me@astr.cc>
2025-02-08 22:46:10 +0100

    more odds fixes

--- resim.py
+++ resim.py
@@ -490,7 +490,9 @@ class Resim:
     
     def start_game_day(self, season, day):
         # happens end of last game day, really
+        self.print(f"=== starting game day s{season+1}d{day+1} ({season,day})")
         current_game_order = self.data.fetch_game_order(season, day)
+        has_mismatch = False
         for game_id in current_game_order: 
             raw_updates = self.data.get_raw_game_updates(game_id)
             predicted_home_pitcher = [u["data"]["homePitcher"] for u in raw_updates if u["data"]["homePitcher"]][0]
@@ -504,7 +506,8 @@ class Resim:
 
             if mismatch:
                 self.print(f"!!! warn: mispredicted pitchers on {season, day}")
-                self.calc_next_game_odds(game_id, use_early_data=False)
+                has_mismatch = True
+                self.calc_next_game_odds(game_id, use_early_data=False, data_invalid=True)
 
         if day >= 99:
             for game_id in current_game_order:
@@ -520,10 +523,10 @@ class Resim:
             upcoming_game_order = self.data.fetch_game_order(season, day+1)
             self.print(f"next day order: {upcoming_game_order}")
             for upcoming_game_id in upcoming_game_order:
-                self.calc_next_game_odds(upcoming_game_id)
+                self.calc_next_game_odds(upcoming_game_id, data_invalid=has_mismatch)
         pass
 
-    def calc_next_game_odds(self, game_id, use_early_data=True):
+    def calc_next_game_odds(self, game_id, use_early_data=True, data_invalid=False):
         # todo: merge this into data.py, it belongs there
         # yes we are intentionally fetching standings for the "previous" day
         # when doing upcoming-game odds, because that's what it'd have available
@@ -534,11 +537,17 @@ class Resim:
 
         game_data = [u['data'] for u in raw_updates if u['data']['homeOdds'] > 0 and u['data']['homePitcher']][0]
         if not use_early_data:
-            game_data = [u['data'] for u in raw_updates if u['data']['gameStart']][0]
+            game_data = [u['data'] for u in raw_updates if u['data']['gameStart'] and u['data']['homePitcher']][0]
         
-        data_known_invalid = False
+        data_known_invalid = data_invalid
         # missing the "early" event so we don't get the wrong odds
         if game_id == "c8bfd47f-3fbb-48fb-a1f7-5c95daf26f81" and use_early_data:
+            data_known_invalid = True
+        if len(self.started_days) < 2:
+            # first rolls of each fragment seem broken, this is our heuristic
+            data_known_invalid = True
+        if self.day in [0, 27, 72, 99]:
+            # skip problematic roll ordering for now
             data_known_invalid = True
 
         home_odds = game_data['homeOdds']
@@ -547,7 +556,7 @@ class Resim:
         self.print("===")
 
         fuzz_roll = self.roll("odds fuzzing")
-        delta = (fuzz_roll-0.5)*0.07
+        delta = (0.03+fuzz_roll*0.07)-0.05
 
         self.print(f"=== matchup: s{game_data['season']+1}d{game_data['day']+1}, game {game_id}, {game_data['awayTeamNickname']}@{game_data['homeTeamNickname']}")
         self.print(f"=== {game_data['awayPitcherName']} @ {game_data['homePitcherName']}")
@@ -561,11 +570,16 @@ class Resim:
         home_team = self.data.get_team(game_data["homeTeam"])
         away_team = self.data.get_team(game_data["awayTeam"])
 
+        # make sure players have right hitting ratings?
+        self.data.fetch_players(self.event['created'])
+
         def batting_stars(p):
-            return ((1 - p.data['tragicness']) ** 0.01) * (p.data['thwackability'] ** 0.35) * (p.data['moxie'] ** 0.075) * (p.data['divinity'] ** 0.35) * (p.data['musclitude'] ** 0.075) * ((1 - p.data['patheticism']) ** 0.05) * (p.data['martyrdom'] ** 0.02)
+            return p.data['hittingRating']
+            # return ((1 - p.data['tragicness']) ** 0.01) * (p.data['thwackability'] ** 0.35) * (p.data['moxie'] ** 0.075) * (p.data['divinity'] ** 0.35) * (p.data['musclitude'] ** 0.075) * ((1 - p.data['patheticism']) ** 0.05) * (p.data['martyrdom'] ** 0.02)
 
         def pitching_stars(p):
-            return (p.data["shakespearianism"] ** 0.1) * (p.data["unthwackability"] ** 0.5) * (p.data["coldness"] ** 0.025) * (p.data["overpowerment"] ** 0.15) * (p.data["ruthlessness"] ** 0.4)
+            return p.data['pitchingRating']
+            # return (p.data["shakespearianism"] ** 0.1) * (p.data["unthwackability"] ** 0.5) * (p.data["coldness"] ** 0.025) * (p.data["overpowerment"] ** 0.15) * (p.data["ruthlessness"] ** 0.4)
 
         home_batting_stars = sum(batting_stars(self.data.get_player(batter_id)) for batter_id in home_team.lineup)
         away_batting_stars = sum(batting_stars(self.data.get_player(batter_id)) for batter_id in away_team.lineup)
@@ -590,7 +604,7 @@ class Resim:
             self.print(f"unambiguous unfuzzed away odds: {unfuzzed_away_odds}")
 
             if not data_known_invalid:
-                self.odds_log.append(OddsLog(game_id=game_id, season=game_data["season"], day=game_data["day"], home_batting_stars=home_batting_stars, away_batting_stars=away_batting_stars, home_pitching_stars=home_pitching_stars, away_pitching_stars=away_pitching_stars, home_wins=home_wins, away_wins=away_wins, home_odds=unfuzzed_home_odds, away_odds=unfuzzed_away_odds, home_batters=len(home_team.lineup), away_batters=len(away_team.lineup), home_pitcher_id=game_data['homePitcher'], away_pitcher_id=game_data['awayPitcher'], home_team=home_team.id, away_team=away_team.id))
+                self.odds_log.append(OddsLog(game_id=game_id, season=game_data["season"], day=game_data["day"], home_batting_stars=home_batting_stars, away_batting_stars=away_batting_stars, home_pitching_stars=home_pitching_stars, away_pitching_stars=away_pitching_stars, home_wins=home_wins, away_wins=away_wins, home_odds=unfuzzed_home_odds, away_odds=unfuzzed_away_odds, home_batters=len(home_team.lineup), away_batters=len(away_team.lineup), home_pitcher_id=game_data['homePitcher'], away_pitcher_id=game_data['awayPitcher'], home_team=home_team.id, away_team=away_team.id, home_team_name=game_data['homeTeamNickname'], away_team_name=game_data['awayTeamNickname'], home_pitcher_name=game_data['homePitcherName'], away_pitcher_name=game_data['awayPitcherName']))
 
             for odd in [unfuzzed_home_odds, unfuzzed_away_odds]:
                 rounded = str(odd)[:10]
@@ -1003,14 +1017,16 @@ class Resim:
             game_id = self.event["gameTags"][0] # state not setup yet
 
             sd = (self.event['season'], self.event['day'])
-            self.print(f"game start: {sd} (zero-indexed)")
+            self.print(f"game start: {sd} (zero-indexed)")  
             if sd not in self.started_days:
                 self.started_days.add(sd)
-                
-                self.start_game_day(self.event['season'], self.event['day'])
 
+                # todo: when does this go before, when does it go after?                
                 for _ in range(extra_start_rolls.get(sd, 0)):
                     self.roll(f"align start {game_id} day {self.day}")
+
+                self.start_game_day(self.event['season'], self.event['day'])
+
 
             return True
         if self.ty in [EventType.PLAY_BALL]:
@@ -4982,6 +4998,10 @@ class OddsLog:
     away_pitcher_id: str
     home_team: str
     away_team: str
+    home_team_name: str
+    away_team_name: str
+    home_pitcher_name: str
+    away_pitcher_name: str
     home_wins: float
     away_wins: float
     home_odds: float
